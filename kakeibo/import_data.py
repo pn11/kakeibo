57006a02f19af6747bdea7271c2b88c3d6b61dfd
oka <pn11@users.noreply.github.com>
2019-09-10 23:00:00 +0900

    improve parsing, add mojimoji, update dictionary

--- kakeibo/import_data.py
+++ kakeibo/import_data.py
@@ -1,13 +1,36 @@
 # coding: utf-8
-
-import re
 import glob
+import logging
+import mojimoji
 import pandas as pd
+import re
+
 from kakeibo.charge_data import ChargeData
 
 rakuten_dir = "data/rakuten"
 jwest_dir = "data/jwest"
 lawson_dir = "data/lawson"
+
+
+invalid_characters = ['㈱']
+
+def parse_number(number_str):
+    '''カンマを含む数値の文字列をfloatに変換'''
+    if type(number_str) == float:
+        # float だった場合はそのまま返す
+        return number_str
+    elif type(number_str) == int:
+        return float(number_str)
+    elif ',' in number_str:
+        number_str = number_str.replace(',', '')
+    return float(number_str)
+
+
+def parse_str(str_):
+    '''カタカナは全角に、英字、数字は半角に統一'''
+    if type(str_) != str:
+        str_ = str(str_)
+    return mojimoji.han_to_zen(mojimoji.zen_to_han(str_, kana=False), digit=False, ascii=False)
 
 
 def import_data(dir, data_type):
@@ -28,7 +51,11 @@ def import_rakuten(dir):
     df = None
 
     for f in filenames:
-        df_tmp = pd.read_csv(f, encoding="Shift-JIS")
+        try:
+            df_tmp = pd.read_csv(f, encoding="Shift-JIS")
+        except UnicodeDecodeError:
+            logging.error(f'{f} に無効な全角文字(例:{invalid_characters})が含まれています。')
+            exit(-1)
         if df is None:
             df = df_tmp
         else:
@@ -55,14 +82,14 @@ def import_rakuten(dir):
         else:
             raise ValueError("未知のCSVフォーマット")
         data.create_date()
-        data.charge = float(row[u"利用金額"])
+        data.charge = parse_number(row[u"利用金額"])
         data.is_new_signature = str(row[u"新規サイン"])
-        data.user = str(row[u"利用者"])
-        data.shop = str(row[u"利用店名・商品名"])
-        data.payment_method = str(row[u"支払方法"])
-        data.charge = float(row[u"利用金額"])
-        data.payment_fee = float(row[u"支払手数料"])
-        data.total_payment_amount = float(row[u"支払総額"])
+        data.user = parse_str(row[u"利用者"])
+        data.shop = parse_str(row[u"利用店名・商品名"])
+        data.payment_method = parse_str(row[u"支払方法"])
+        data.charge = parse_number(row[u"利用金額"])
+        data.payment_fee = parse_number(row[u"支払手数料"])
+        data.total_payment_amount = parse_number(row[u"支払総額"])
 
         datalist.append(data)
 
@@ -94,10 +121,10 @@ def import_jwest(dir):
         data.month = int(split[1])
         data.day = int(split[2])
         data.create_date()
-        data.user = str(row[u"ご利用者"])
-        data.shop = str(row[u"ご利用店名（海外ご利用店名／海外都市名）"])
-        data.charge = float(row[u"ご利用金額（円）"].replace(',', ''))
-        data.notes = str(row[u"現地通貨額・通貨名称・換算レート"])
+        data.user = parse_str(row[u"ご利用者"])
+        data.shop = parse_str(row[u"ご利用店名（海外ご利用店名／海外都市名）"])
+        data.charge = parse_number(row[u"ご利用金額（円）"])
+        data.notes = parse_str(row[u"現地通貨額・通貨名称・換算レート"])
 
         datalist.append(data)
 
@@ -129,10 +156,10 @@ def import_lawson(dir):
         data.month = int(split[1])
         data.day = int(split[2])
         data.create_date()
-        data.user = str(row[u"本人・家族区分"])
-        data.shop = str(row[u"ご利用店名及び商品名"])
-        data.charge = float(row[u"利用金額"])
-        data.notes = str(row[u"備考"])
+        data.user = parse_str(row[u"本人・家族区分"])
+        data.shop = parse_str(row[u"ご利用店名及び商品名"])
+        data.charge = parse_number(row[u"利用金額"])
+        data.notes = parse_str(row[u"備考"])
 
         datalist.append(data)
 
